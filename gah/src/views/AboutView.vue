2b4bda7964cbb86d05fd13a5d73f1ae3a1cb2ccb
Nathan Chen <113192742+seribical@users.noreply.github.com>
2024-02-12 13:12:25 -0500

    c

--- gah/src/views/AboutView.vue
+++ gah/src/views/AboutView.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <button @click="store.count++">
+    From B: {{ store.count }}
+  </button>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+<script setup>
+  import {store} from "../stores/store"
+</script>
 
+<style lang="scss" scoped>
 
 </style>
